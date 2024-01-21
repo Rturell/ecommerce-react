699115ea16a856db914c41a69af43a13d3d0014b
rodrigoturell <rodrigoturell@gmail.com>
2024-01-21 17:02:45 -0300

    create navBar

--- src/components/NavBar/NavBar.jsx
+++ src/components/NavBar/NavBar.jsx
@@ -1,6 +1,19 @@
+import { LuShoppingCart } from "react-icons/lu";
+
 function NavBar() {
   return (
-    <h1>Hola</h1>
+    <>
+      <nav>
+        <div className="flex flex-row gap-10">
+          <h2>ecommerce</h2>
+          <a href="">Link 1</a>
+          <a href="">Link 2</a>
+          <a href="">Link 3</a>
+          <a href="">Link 4</a>
+          <LuShoppingCart /> 
+        </div>
+      </nav>
+    </>
   );
 }
 
